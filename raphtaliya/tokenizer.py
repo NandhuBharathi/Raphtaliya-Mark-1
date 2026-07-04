f539729d2d12e8020e574f0d5621d395eeb2abe7
NANDHAKUMAR M <nandhubharathimpt@gmail.com>
2026-07-04 02:05:39 +0000

    Upgrade Tokenizer V2

--- raphtaliya/tokenizer.py
+++ raphtaliya/tokenizer.py
@@ -1,4 +1,3 @@
-
 from raphtaliya.normalizer import Normalizer
 from raphtaliya.pretokenizer import PreTokenizer
 from raphtaliya.vocabulary import Vocabulary
@@ -12,18 +11,31 @@ class Tokenizer:
         self.pretokenizer = PreTokenizer()
         self.vocabulary = vocabulary
 
-    def encode(self, text):
+    def tokenize(self, text):
 
         text = self.normalizer.normalize(text)
 
-        tokens = self.pretokenizer.tokenize(text)
+        return self.pretokenizer.tokenize(text)
 
-        ids = [
+    def detokenize(self, tokens):
+
+        return " ".join(tokens)
+
+    def encode(self, text):
+
+        tokens = self.tokenize(text)
+
+        return [
             self.vocabulary.get_id(token)
             for token in tokens
         ]
 
-        return ids
+    def encode_batch(self, texts):
+
+        return [
+            self.encode(text)
+            for text in texts
+        ]
 
     def decode(self, ids):
 
@@ -32,4 +44,36 @@ class Tokenizer:
             for token_id in ids
         ]
 
-        return " ".join(tokens)
+        return self.detokenize(tokens)
+
+    def decode_batch(self, batch_ids):
+
+        return [
+            self.decode(ids)
+            for ids in batch_ids
+        ]
+
+    def contains(self, token):
+
+        return token in self.vocabulary.word_to_id
+
+    def vocab_size(self):
+
+        return self.vocabulary.size()
+
+    def statistics(self):
+
+        return {
+            "vocabulary_size": self.vocabulary.size(),
+            "special_tokens": self.vocabulary.special_tokens
+        }
+
+
+if __name__ == "__main__":
+
+    from raphtaliya.utils import show_upgrade
+
+    show_upgrade(
+        module="Tokenizer",
+        version="V2.0"
+    )
